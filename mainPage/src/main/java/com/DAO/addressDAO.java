204fc8ff0e1042db8447a829a4f3f1ac0849f202
CGI <dltmdfud0407@naver.com>
2021-06-09 16:14:19 +0900

    addressDAO 이승령

--- mainPage/src/main/java/com/DAO/addressDAO.java
+++ mainPage/src/main/java/com/DAO/addressDAO.java
@@ -1,5 +1,53 @@
 package com.DAO;
 
-public class addressDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-}
+import com.VO.positiveVO;
+
+import java.sql.DriverManager;
+
+
+
+public class addressDAO {
+	
+	public ArrayList<positiveVO> address() {
+		String driver = "oracle.jdbc.OracleDriver";
+		String dbUrl = "jdbc:oracle:thin:@146.56.169.187:1521:xe";
+		ArrayList<positiveVO> list = new ArrayList<positiveVO>();
+					
+		try {
+			Class.forName(driver);
+			//System.out.println("DB Driver loading");
+			Connection con = DriverManager.getConnection(dbUrl,"smarttrand","smarttrand");
+			//System.out.println("DB Connection:" + con);
+			String sql = "select * from POSITIVEATTRACTIONLIST";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) { 
+				positiveVO vo = new positiveVO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4));
+				list.add(vo);
+				
+				
+			}
+			rs.close();
+			pstmt.close();
+			con.close();
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+			
+		
+		return list;
+		
+		}
+	}
+
