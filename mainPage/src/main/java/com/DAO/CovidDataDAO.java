3683af1d27e88be7acd5dd82a5625533cb0823d0
CGI <CGI@59.28.30.47>
2021-06-09 16:49:08 +0900

    CovidDataDAO 16:49

--- mainPage/src/main/java/com/DAO/CovidDataDAO.java
+++ mainPage/src/main/java/com/DAO/CovidDataDAO.java
@@ -5,23 +5,26 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.VO.covidDataVO;
 
 public class CovidDataDAO {
 
-	public static void main(String[] args) {
-		String driver = "oracle.jdbc.OracleDriver";
-		String dbUrl = "jdbc:oracle:thin:@146.56.169.187:1521:xe";
+	public ArrayList<covidDataVO> covidDataList() {
+		
+		ArrayList<covidDataVO> list = new ArrayList<covidDataVO>();
+			String dbUrl = "jdbc:oracle:thin:@146.56.169.187:1521:xe";
 		try {
-			Class.forName(driver);
-			//System.out.println("DB Driver loading");
+			Class.forName("oracle.jdbc.OracleDriver");
+			System.out.println("DB Driver loading");
 			Connection con = DriverManager.getConnection(dbUrl,"smarttrand","smarttrand");
-			//System.out.println("DB Connection:" + con);
 			String sql = "SELECT * FROM COVIDDATA";
 			PreparedStatement pstmt = con.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
+				ResultSet rs = pstmt.executeQuery();
 			while(rs.next()) { 
-				System.out.println(
-						rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5)+" "+rs.getString(6));
+				covidDataVO vo = new covidDataVO(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6));	
+				list.add(vo);	
 			}
 			rs.close();
 			pstmt.close();
@@ -32,7 +35,9 @@ public class CovidDataDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
+		return list;
+	
 }
+}
+
 
