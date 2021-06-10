bd6f5a537798da583d6be6f1183eb489258c1a74
CGI <dltmdfud0407@naver.com>
2021-06-10 11:24:25 +0900

    코로나 데이터 수정

--- mainPage/src/main/java/com/DAO/CovidDataDAO.java
+++ mainPage/src/main/java/com/DAO/CovidDataDAO.java
@@ -5,25 +5,35 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.VO.covidDataVO;
 
-public class CovidDataDAO {
 
-	public static void main(String[] args) {
+import java.sql.DriverManager;
+
+
+
+public class CovidDataDAO {
+	public ArrayList<covidDataVO> address() {
 		String driver = "oracle.jdbc.OracleDriver";
 		String dbUrl = "jdbc:oracle:thin:@146.56.169.187:1521:xe";
+		ArrayList<covidDataVO> list = new ArrayList<covidDataVO>();
+					
 		try {
 			Class.forName(driver);
 			//System.out.println("DB Driver loading");
 			Connection con = DriverManager.getConnection(dbUrl,"smarttrand","smarttrand");
 			//System.out.println("DB Connection:" + con);
-			String sql = "SELECT * FROM COVIDDATA";
+			String sql = "select * from COVIDDATA";
 			PreparedStatement pstmt = con.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
+			
 			while(rs.next()) { 
-				covidDataVO vo = new covidDataVO(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5)+" "+rs.getString(6));
-						
+				covidDataVO vo = new covidDataVO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+				list.add(vo);
+
+				
 			}
 			rs.close();
 			pstmt.close();
@@ -34,7 +44,11 @@ public class CovidDataDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+			
+		
+		return list;
+		
+		}
 	}
 
-}
 
